993cbe9a8036dcdda096c6e968f36af7527d0080
Alan Kusparmakov <alan.kusparmakov@gmail.com>
2026-09-04 16:18:39 -0700

    fixed out and in

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,37 +1,51 @@
 #include "main.h"
 
 #include "stdbool.h"
+#include "stm32l476xx.h"
+#include "stm32l4xx_hal_gpio.h"
 #include <stdint.h>
 
 /* On the Keypad, from left to right: Row1, Row2, Row3, Row4, Col.1, Col.2, Col.3
 */
 
+#define ROW_COUNT 4
+#define COL_COUNT 3
+
 #define PC_ROW_START 0
 #define PC_COLUMN_START 10
 
-void gpio_init() {
+void gpio_init()
+{
+	// set rows as input
+	// PC0-3
+	GPIOC->MODER &= ~(GPIO_MODER_MODE0 | GPIO_MODER_MODE1 | GPIO_MODER_MODE2 | GPIO_MODER_MODE3);
 
+	// set cols as output
+	// PC10-12
+	GPIOC->MODER &= ~(GPIO_MODER_MODE10 | GPIO_MODER_MODE11 | GPIO_MODER_MODE12);
+	GPIOC->MODER |= (GPIO_MODER_MODE10_0 | GPIO_MODER_MODE11_0 | GPIO_MODER_MODE12_0);
+}
+bool row_is_high(uint8_t i)
+{
+	return GPIOC->IDR & (1 << (PC_ROW_START + i));
 }
 
-bool row_is_high(uint8_t i) {
-	return (GPIOC->IDR & (PC_ROW_START << i));
+void col_set(uint8_t j, bool high)
+{
+	GPIOC->ODR &= ~(1 << (PC_COLUMN_START + j));
+	if (high) 
+		GPIOC->ODR |= (1 << (PC_COLUMN_START + j));
 }
 
-void col_set(uint8_t j, bool high) {
-
-}
-
-
-bool key_is_pressed(uint8_t i, uint8_t j) {
-
+bool key_is_pressed(uint8_t i, uint8_t j)
+{
 }
 
 int main(void)
 {
-    HAL_Init();
+	HAL_Init();
 
-    while (1) {
-        /* Add application code here. */
-    }
-
+	while (1) {
+		/* Add application code here. */
+	}
 }
