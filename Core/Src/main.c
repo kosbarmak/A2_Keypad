3ff104811bcec65bdc646321c5d60b4c58439188
Alan Kusparmakov <alan.kusparmakov@gmail.com>
2026-09-04 16:40:17 -0700

    loop over keys

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,11 +20,16 @@ void gpio_init()
 	// PC0-3
 	GPIOC->MODER &= ~(GPIO_MODER_MODE0 | GPIO_MODER_MODE1 | GPIO_MODER_MODE2 | GPIO_MODER_MODE3);
 
+	// set rows as pull-down
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD0 | GPIO_PUPDR_PUPD1 | GPIO_PUPDR_PUPD2 | GPIO_PUPDR_PUPD3);
+	GPIOC->PUPDR |= (GPIO_PUPDR_PUPD0_1 | GPIO_PUPDR_PUPD1_1 | GPIO_PUPDR_PUPD2_1 | GPIO_PUPDR_PUPD3_1);
+
 	// set cols as output
 	// PC10-12
 	GPIOC->MODER &= ~(GPIO_MODER_MODE10 | GPIO_MODER_MODE11 | GPIO_MODER_MODE12);
 	GPIOC->MODER |= (GPIO_MODER_MODE10_0 | GPIO_MODER_MODE11_0 | GPIO_MODER_MODE12_0);
 }
+
 bool row_is_high(uint8_t i)
 {
 	return GPIOC->IDR & (1 << (PC_ROW_START + i));
@@ -33,12 +38,41 @@ bool row_is_high(uint8_t i)
 void col_set(uint8_t j, bool high)
 {
 	GPIOC->ODR &= ~(1 << (PC_COLUMN_START + j));
-	if (high) 
+	if (high)
 		GPIOC->ODR |= (1 << (PC_COLUMN_START + j));
 }
 
-bool key_is_pressed(uint8_t i, uint8_t j)
+void cols_set_low()
 {
+	for (uint8_t j = 0; j < COL_COUNT; j++) {
+		col_set(j, false);
+	}
+}
+
+void col_isolate(uint8_t j)
+{
+	cols_set_low();
+	col_set(j, true);
+}
+
+bool key_is_pressed(uint8_t row, uint8_t col)
+{
+	col_isolate(col);
+	return row_is_high(row);
+}
+
+uint8_t get_key_value(uint8_t i, uint8_t j) {
+	// regular keys
+	if ((0 < i) && (i < ROW_COUNT) && (0 < j) && (j < COL_COUNT - 1))
+		return (i * COL_COUNT) + j;
+}
+
+uint8_t get_pressed_key()
+{
+	for (uint8_t i = 0; i < COL_COUNT; i++)
+		for (uint8_t j = 0; j < ROW_COUNT - 1; j++)
+			if (key_is_pressed(i, j))
+				get_key_value(i, j);
 }
 
 int main(void)
@@ -46,6 +80,6 @@ int main(void)
 	HAL_Init();
 
 	while (1) {
-		/* Add application code here. */
+		get_pressed_key();
 	}
 }
