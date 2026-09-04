3595546edb0e4d4faf2acd00d17336355a95f8b8
Alan Kusparmakov <alan.kusparmakov@gmail.com>
2026-09-04 16:03:03 -0700

    row change

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -9,16 +9,22 @@
 #define PC_ROW_START 0
 #define PC_COLUMN_START 10
 
+void gpio_init() {
+
+}
+
 bool row_is_high(uint8_t i) {
 	return (GPIOC->IDR & (PC_ROW_START << i));
 }
 
-bool col_is_high(uint8_t j) {
-	return (GPIOC->IDR & (PC_COLUMN_START << j));
+void col_set(uint8_t j, bool high) {
+
 }
 
 
+bool key_is_pressed(uint8_t i, uint8_t j) {
 
+}
 
 int main(void)
 {
